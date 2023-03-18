f992a6522ddfe97a92512c95b8f7b75acd0513b5
Abhishek Pashte <90621734+Abhishek-Pashte@users.noreply.github.com>
2023-03-18 09:53:44 +0530

    Add files via upload

---
+++ Half_Pyramid_Using_Numbers.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=0;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            if((i+j)>n)
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}
---
+++ Inverted_Pattern.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=0;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i;j++)
+        {
+            cout<<j+1<<" ";
+        }
+        cout<<endl;
+    }
+
+    for(int i=n;i>0;i--)
+    {
+        for(int j=0;j<i;j++){
+            cout<<j+1<<" ";
+        }
+        cout<<endl;
+    }
+}
